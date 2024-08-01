e01217e783e759a928453e173733d4245acf25ae
Roque Santos <roque-100@hotmail.com>
2024-08-01 01:07:03 -0300

    added mouse look

--- scripts/characters/Player.cs
+++ scripts/characters/Player.cs
@@ -3,10 +3,34 @@ using System;
 
 public partial class Player : Entity
 {
+	private static float MAX_HEAD_ROTATION = 89.0f;
 
-	public override void _PhysicsProcess(double delta)
-	{
+	[ExportGroup("Mouse and Keyboard")]
+	[Export(PropertyHint.Range, "0.1, 5.0, 0.1")] private float sensitivity = .1f;
+
+	#region Nodes
+	private Node3D head;
+    #endregion
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+
+		head = GetNode<Node3D>("Head");
+    }
+
+    public override void _Input(InputEvent @event)
+    {
 		HandleInput();
+		if (@event is InputEventMouseMotion mouseEvent) {
+        	Look(mouseEvent.Relative);
+		}
+    }
+
+    public override void _PhysicsProcess(double delta)
+	{
 
 		base._PhysicsProcess(delta);
 	}
@@ -21,5 +45,18 @@ public partial class Player : Entity
 		{
 			motion.Y = jumpVelocity;
 		}
+	}
+
+	private void Look(Vector2 direction) {
+		// Rotates player
+		RotateY(-Mathf.DegToRad(direction.X * sensitivity));
+
+		// Rotates head
+		head.RotateX(-Mathf.DegToRad(direction.Y * sensitivity));
+		head.RotationDegrees = new Vector3(
+			Mathf.Clamp(head.RotationDegrees.X, -MAX_HEAD_ROTATION, MAX_HEAD_ROTATION),
+			head.Rotation.Y,
+			head.Rotation.Z
+		);
 	}
 }
